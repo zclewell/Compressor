9420f653f4bdd20f02ab2144bda7b20a5ec087af
Aditya Pillai <aditya.pillai53916@gmail.com>
2017-11-02 15:28:56 -0500

    added multithreading to runlength

--- mainRunLength.c
+++ mainRunLength.c
@@ -5,13 +5,30 @@
 
 
 
+
+//used for pushing onto queue
+typedef struct{
+  char* line;
+  size_t lineNo;
+} fileLine;
+
+pthread_mutex_t lock;
+
+
 queue* queue;
+vector* results;
 
-void* encodeLine(void* line){
-
-
-
-
+void* encodeLine(){
+  fileLine* data;
+  while((data = queue_pull(queue))){
+    size_t lineNum = data->lineNo;
+    char* encodedLine = encode(data->line);
+    vector_set(results, lineNum, encodeLine);
+    free(data->line);
+    free(data);
+  }
+  queue_push(NULL);
+  return NULL;
 }
 
 
@@ -19,12 +36,13 @@ void* encodeLine(void* line){
 
 int main(int argc, char**argv){
   if(argc < 2){
-    fprintf(stderr, "usage: mainRunLength input_file outout_file [num_threads]\n", );
+    fprintf(stderr, "usage compress: %s input_file outout_file [num_threads]\n",argv[0]);
     return 0;
   }
 
 
   queue = queue_create(-1);
+  results = string_vector_create();
   size_t num_threads = 0;
   if(argc >= 3){
     num_threads = atoi(argv[2]);
@@ -37,16 +55,46 @@ int main(int argc, char**argv){
   }
 
   size_t i;
-  pthrad_t threads[num_threads];
+  pthread_t threads[num_threads];
   for(i = 0; i < num_threads; ++i){
-    int* threadNo = malloc(sizeof(int));
-    *threadNo = i;
-    int t = pthrad_create(threads + i, 0, encodeLine, 0);
+    int t = pthread_create(threads + i, 0, encodeLine, 0);
     if(!t){
       fprintf(stderr, "Count not create thread\n");
       exit(1);
     }
   }
+
+  FILE*in = fopen(argv[1], "r");
+  size_t bytesRead;
+  char* line = NULL;
+  size_t n = 0;
+  size_t lineNum = 0;
+  while((bytesRead = getline(in, line, n)) != -1){
+    fileLine* temp = malloc(sizeof(fileLine));
+    temp->lineNo = lineNum++;
+    temp->line = strdup(line);
+    queue_push(queue, temp);
+  }
+  free(line);
+  queue_push(NULL);
+
+
+
+  for(i = 0;i < num_threads; ++i){
+    pthread_join(threads[i], 0);
+  }
+
+  FILE* out = fopen(argv[2],"w");
+  for(i = 0; i < lineNum; ++i){
+    char* resLine = vector_get(results, i);
+    fprintf(out, "%s\n", resLine);
+  }
+
+
+  //TODO clean up memory
+
+
+
 
 
 
