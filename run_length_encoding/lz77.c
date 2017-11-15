a396aea185a9b030b51a601c29cf2438db9cd6b4
Aditya Pillai <aditya.pillai53916@gmail.com>
2017-11-15 14:15:04 -0600

    basic outline of encode function

--- run_length_encoding/lz77.c
+++ run_length_encoding/lz77.c
@@ -1,7 +1,7 @@
 #include <glib.h>
 #include <stdlib.h>
 
-
+/*
 void populateDictionary(GHashTable* table, char* str){
   size_t i =0;
   while(i < strlen(str)){
@@ -18,4 +18,39 @@ void populateDictionary(GHashTable* table, char* str){
     str[j] = saved;
     i = j;
   }
+}*/
+
+int asciiSum(char* str){
+  int sum = 0;
+  while(*str){
+    sum += (int)*str;
+    ++str;
+  }
+  return sum;
+}
+
+
+char* encode(char* input, int windowsSize, int searchSize){
+
+  int nextSymIncrement = 2;
+  int currentIndex  = 0;
+  int nextIndex = 1;
+  //char current = *input;
+  char saved = input[nextIndex + 1];
+  input[nextIndex + 1] = 0;
+  if(g_hash_table_contains(input + currentIndex)){
+    ++nextIndex;
+    continue;
+  } else{
+    g_hash_table_add(strdup(input + currentIndex), table);
+  }
+  char temp = input[nextIndex];
+  input[nextIndex] = 0;
+  sprintf(STR HERE, "%d",asciiSum(input + currentIndex));
+  // restore string
+  input[nextIndex] = temp;
+  input[nextIndex + 1] = saved;
+  currentIndex = nextIndex;
+  nextIndex = currentIndex + 1;
+
 }
