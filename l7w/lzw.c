7f30e2cd6872408dcdbb45b6105feb7fff0c5a0c
Aditya Pillai <aditya.pillai53916@gmail.com>
2017-11-29 14:09:27 -0600

    encode function for lzw, uses bitfile library

--- l7w/lzw.c
+++ l7w/lzw.c
@@ -1,56 +1,60 @@
 #include <glib.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "bitfile.h"
 
-/*
-void populateDictionary(GHashTable* table, char* str){
-  size_t i =0;
-  while(i < strlen(str)){
-    size_t j;
-    char saved;
-    for(j = i + 1; j < streln(str); ++j){
-      saved = str[j];
-      str[j] = 0;
-      if(!g_hash_table_contains(table, str + i)){
-        g_hash_table_add(strdup(str + i), table);
-        break;
-      }
-    }
-    str[j] = saved;
-    i = j;
-  }
-}*/
-
-int asciiSum(char* str){
-  int sum = 0;
-  while(*str){
-    sum += (int)*str;
-    ++str;
-  }
-  return sum;
-}
+int BITSIZE = 12;
+int extendedAscii = 256;
 
 
-char* encode(char* input, int windowsSize, int searchSize){
-
-  int nextSymIncrement = 2;
+void encode(char* input, bitfile_t* file){
+  int length = strlen(input);
+  //char* result = malloc(length);
+  GHashTable* table = g_hash_table_new(g_str_hash, g_str_equal);
   int currentIndex  = 0;
   int nextIndex = 1;
-  //char current = *input;
-  char saved = input[nextIndex + 1];
-  input[nextIndex + 1] = 0;
-  if(g_hash_table_contains(input + currentIndex)){
-    ++nextIndex;
-    continue;
-  } else{
-    g_hash_table_add(strdup(input + currentIndex), table);
-  }
-  char temp = input[nextIndex];
-  input[nextIndex] = 0;
-  sprintf(STR HERE, "%d",asciiSum(input + currentIndex));
-  // restore string
-  input[nextIndex] = temp;
-  input[nextIndex + 1] = saved;
-  currentIndex = nextIndex;
-  nextIndex = currentIndex + 1;
+  //char* output = result;
 
+  while(nextIndex <= length){
+    char saved = 0;
+    if(nextIndex != length){
+      saved = input[nextIndex + 1];
+      input[nextIndex + 1] = 0;
+    }
+
+    if(g_hash_table_contains(table, input + currentIndex)){
+      input[++nextIndex] = saved;
+      continue;
+    } else{
+      g_hash_table_add(table, strdup(input + currentIndex));
+    }
+    char temp = input[nextIndex];
+    input[nextIndex] = 0;
+    if(strlen(input + currentIndex) > 1){
+      BitFilePutBitsNum(file, &extendedAscii, BITSIZE, sizeof(int));
+      //memcpy(output, &extendedAscii, 2);
+      ++extendedAscii;
+    } else{
+      BitFilePutBitsNum(file, input + currentIndex, BITSIZE, 1);
+      //memcpy(output, input + currentIndex, 2);
+    }
+    //output += 2;
+
+    // restore string
+    input[nextIndex] = temp;
+    input[nextIndex + 1] = saved;
+    currentIndex = nextIndex;printf("%s\n", );
+    nextIndex = currentIndex + 1;
+  }
+
+  g_hash_table_destroy(table);
+  //return result;
+}
+
+int main (int argc, char** argv){
+  bit_file_t* inputFile = BitFileOpen(argv[1], BF_READ);
+  bitfile_t* outFile = BitFileOpen(argv[2], BF_WRITE);
+  encode(strdup("this is the"), outFile);
+  return 0;
 }
